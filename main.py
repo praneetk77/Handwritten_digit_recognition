bea49a5980938c244f71952c3ce243dec942a8f4
praneetk77 <praneetkarna77@gmail.com>
2022-08-19 15:34:56 +0530

    Saved model and added readme

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ from tensorflow import keras
 data = keras.datasets.mnist
 (x_train,y_train),(x_test,y_test) = data.load_data()
 
-x_train = keras.utils.normalize(x_train, axis=1)
-x_test = keras.utils.normalize(x_test, axis=1)
+# x_train = keras.utils.normalize(x_train, axis=1)
+# x_test = keras.utils.normalize(x_test, axis=1)
+#
+# model = keras.models.Sequential()
+# model.add(keras.layers.Flatten(input_shape=(28,28)))
+# model.add(keras.layers.Dense(128, activation="relu"))
+# model.add(keras.layers.Dense(128, activation="relu"))
+# model.add(keras.layers.Dense(10, activation="softmax"))
+#
+# model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+#
+# model.fit(x_train, y_train, epochs=3)
+#
+# model.save("handwritten.model")
 
-model = keras.models.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28,28)))
-model.add(keras.layers.Dense(128, activation="relu"))
-model.add(keras.layers.Dense(128, activation="relu"))
-model.add(keras.layers.Dense(10, activation="softmax"))
-
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-
-model.fit(x_train, y_train, epochs=3)
-
-model.save("handwritten.model")
-
-# model = keras.models.load_model("handwritten.model")
+model = keras.models.load_model("handwritten.model")
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print(loss, accuracy)
